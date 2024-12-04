924b0856bacd001577c410d69bd89f77cd3c1add
mani1509 <gajulamani10@gmail.com>
2024-12-04 22:34:25 +0530

    updating app.py

--- app.py
+++ app.py
@@ -1,28 +1,18 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-from models.model_1 import db
-from auth import auth_blueprint
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Configuration
-app.config['SECRET_KEY'] = '5f4e4b8ed9731a35b54d401b9cfa32b3'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Initialize Extensions
-db.init_app(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-# Register Blueprints
-app.register_blueprint(auth_blueprint)
-
-# Home route
-@app.route("/")
-def home():
-    return "Welcome to the app! Go to /login or /signup"
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
