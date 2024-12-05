755aa5ad41c2dcefa0a45e651d1a6078c5ce4eb5
mani1509 <gajulamani10@gmail.com>
2024-12-06 00:08:11 +0530

    updating app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template ,send_from_directory
 
 app = Flask(__name__)
 
@@ -13,6 +13,21 @@ def login():
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@app.route('/test')
+def test():
+    return render_template('3Dtest.html')
+
+@app.route('/static/<path:path>')
+def static_proxy(path):
+    # Ensure Flask sets the correct MIME type for .js files
+    if path.endswith('.js'):
+        return send_from_directory('static', path, mimetype='application/javascript')
+    return send_from_directory('static', path)
+
+@app.route('/node_modules/<path:path>')
+def node_modules(path):
+    return send_from_directory('node_modules', path)
 
 if __name__ == "__main__":
     app.run(debug=True)
